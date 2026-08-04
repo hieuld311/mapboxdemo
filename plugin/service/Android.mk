93d4c7c4ca8b9b373aeecb26fddf8e9145898774
hieuld311 <hieuld311@gmail.com>
2026-08-04 14:33:46 +0700

    Clean plugin makefile whitespace

--- plugin/service/Android.mk
+++ plugin/service/Android.mk
@@ -8,7 +8,7 @@ LOCAL_AIDL_INCLUDES := $(LOCAL_PATH)/src
 LOCAL_MODULE := com.fauto.car.plugin.navigation
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 LOCAL_MODULE_TAGS := optional
-LOCAL_STATIC_JAVA_LIBRARIES := navigation-lib gson-2.9.0 converter-gson-2.9.0 okhttp-3.14.9 okio-1.17.2 retrofit-2.9.0 
+LOCAL_STATIC_JAVA_LIBRARIES := navigation-lib gson-2.9.0 converter-gson-2.9.0 okhttp-3.14.9 okio-1.17.2 retrofit-2.9.0
 LOCAL_JAVA_LIBRARIES := \
         fauto.car.plugin.navigation\
 	android.car\
